7348640c462d67dab28f249b095892e40ab82407
Augsuto Sandrini <augustosandrini2@gmail.com>
2022-10-25 22:34:49 -0300

    classes User e UserRepository, Role e RoleRepository modificadas

--- src/main/java/com/sandrini/leadership/repository/RoleRepository.java
+++ src/main/java/com/sandrini/leadership/repository/RoleRepository.java
@@ -1,4 +1,11 @@
 package com.sandrini.leadership.repository;
 
-public interface RoleRepository {
+import com.sandrini.leadership.domains.ERole;
+import com.sandrini.leadership.domains.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(ERole name);
 }
